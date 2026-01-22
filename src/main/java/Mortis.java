4539ed4e0ea198f4475ca085f0c5a099d493af32
Natalia <nzhao1113@gmail.com>
2026-01-22 16:48:29 +0800

    renamed to Mortis

---
+++ src/main/java/Mortis.java
@@ -0,0 +1,9 @@
+
+public class Mortis {
+    public static void main(String[] args) {
+        //Scanner sc = new Scanner(System.in);
+
+        System.out.println("Hello, I'm Mortis.\nWhat can I do for you today?");
+        System.out.println("Goodbye, user.");
+    }
+}
