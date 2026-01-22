4d9c1713ddb24e65a6c6303b4f3f2b219da85511
Natalia <nzhao1113@gmail.com>
2026-01-22 16:55:21 +0800

    Echo user messages

--- src/main/java/Mortis.java
+++ src/main/java/Mortis.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
 
 public class Mortis {
     public static void main(String[] args) {
-        //Scanner sc = new Scanner(System.in);
-
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hello, I'm Mortis.\nWhat can I do for you today?");
+
+        String userInput = sc.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println(userInput);
+            userInput = sc.nextLine();
+        }
+
         System.out.println("Goodbye, user.");
     }
 }
