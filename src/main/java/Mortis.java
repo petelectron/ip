5335c24c1ed3cbb8e74736db51401782dfd3f63e
Natalia <nzhao1113@gmail.com>
2026-01-22 17:20:47 +0800

    Added task class and mark as done feature

--- src/main/java/Mortis.java
+++ src/main/java/Mortis.java
@@ -4,19 +4,31 @@ import java.util.Scanner;
 public class Mortis {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> ls = new ArrayList<>();
+        ArrayList<Task> ls = new ArrayList<>();
 
-        System.out.println("Hello, I'm Mortis.\nWhat can I do for you today?");
+        System.out.println("Hello, I'm Mortis.\nI'm case sensitive, please be nice to me.");
 
         String userInput = sc.nextLine();
         while (!userInput.equals("bye")) {
             if (userInput.equals("list")) {
                 for (int i = 0; i < ls.size(); i++) {
-                    System.out.println((i + 1) + ". " + ls.get(i));
+                    System.out.println((i + 1) + "." + ls.get(i).toString());
                 }
+            } else if (userInput.startsWith("mark")) {
+                int index = Integer.parseInt(userInput.split(" ")[1]) - 1;
+                ls.get(index).markAsDone();
+                System.out.println("""
+                                   I have marked this task as done:
+                                   """ + ls.get(index).toString());
+            } else if (userInput.startsWith("unmark")) {
+                int index = Integer.parseInt(userInput.split(" ")[1]) - 1;
+                ls.get(index).unmarkAsDone();
+                System.out.println("""
+                                   I have marked this task as not done:
+                                   """ + ls.get(index).toString());
             } else {
                 System.out.println("added: " + userInput);
-                ls.add(userInput);
+                ls.add(new Task(userInput));
             }
             userInput = sc.nextLine();
         }
