2191f41a169c65d8589e4238ad49c80276691e6a
Natalia <nzhao1113@gmail.com>
2026-01-22 17:02:26 +0800

    Added list

--- src/main/java/Mortis.java
+++ src/main/java/Mortis.java
@@ -1,13 +1,23 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Mortis {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> ls = new ArrayList<>();
+
         System.out.println("Hello, I'm Mortis.\nWhat can I do for you today?");
 
         String userInput = sc.nextLine();
         while (!userInput.equals("bye")) {
-            System.out.println(userInput);
+            if (userInput.equals("list")) {
+                for (int i = 0; i < ls.size(); i++) {
+                    System.out.println((i + 1) + ". " + ls.get(i));
+                }
+            } else {
+                System.out.println("added: " + userInput);
+                ls.add(userInput);
+            }
             userInput = sc.nextLine();
         }
 
