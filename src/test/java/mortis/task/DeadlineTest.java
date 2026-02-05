fd6eefa72be51b33e7e37a8e28287637271cf257
Natalia <nzhao1113@gmail.com>
2026-02-05 15:01:48 +0800

    Add JavaDoc headings for test classes

--- src/test/java/mortis/task/DeadlineTest.java
+++ src/test/java/mortis/task/DeadlineTest.java
@@ -5,13 +5,22 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+/**
+ * Tests the Deadline class.
+ */
 public class DeadlineTest {
 
+    /**
+     * Tests correctness of toString() output.
+     */
     @Test
     public void toStringConvention() {
         assertEquals("[D][ ] hi (by: Oct 10 18:00, 2020)", new Deadline("hi", "2020-10-10 1800").toString());
     }
 
+    /**
+     * Tests correctness of toDataString() output.
+     */
     @Test
     public void toDataStringConvention() {
         assertEquals("D, 0, hi, 2020-10-10 1800", new Deadline("hi", "2020-10-10 1800").toDataString());
--- src/test/java/mortis/task/TodoTest.java
+++ src/test/java/mortis/task/TodoTest.java
@@ -5,18 +5,21 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
-
+/**
+ * Tests the Todo class.
+ */
 public class TodoTest {
-    @Test
-    public void dummyTest() {
-        assertEquals(2, 2);
-    }
-
+    /**
+     * Tests correctness of toString() output.
+     */
     @Test
     public void toStringConvention() {
         assertEquals("[T][ ] hi", new Todo("hi").toString());
     }
 
+    /**
+     * Tests correctness of toDataString() output.
+     */
     @Test
     public void toDataStringConvention() {
         assertEquals("T, 0, hi", new Todo("hi").toDataString());
