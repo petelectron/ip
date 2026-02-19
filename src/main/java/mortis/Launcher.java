e2d2c235ad1ca66947057fd4db3c0020db5e1212
Natalia <nzhao1113@gmail.com>
2026-02-20 06:24:06 +0800

    Add jar file

--- src/main/java/mortis/Launcher.java
+++ src/main/java/mortis/Launcher.java
@@ -3,7 +3,7 @@ package mortis;
 import javafx.application.Application;
 
 /**
- * A launcher class to workaround classpath issues.
+ * A launcher class for Mortis.
  */
 public class Launcher {
     public static void main(String[] args) {
